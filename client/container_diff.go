92ed737cd3640718b8f27f7095bb20505e5daf31
Junjun Li <junjunli666@gmail.com>
2019-06-10 15:27:49 +0800

    send url error
    Signed-off-by: Junjun Li <junjunli666@gmail.com>

--- client/container_diff.go
+++ client/container_diff.go
@@ -2,15 +2,12 @@ package client
 
 import (
 	"context"
-	"net/url"
 )
 
 // ContainerDiff returns the file diffs of containers.
 func (client *APIClient) ContainerDiff(ctx context.Context, name string) ([]string, error) {
-	q := url.Values{}
-	q.Set("name", name)
+	resp, err := client.get(ctx, "/containers/"+name+"changes", nil, nil)
 
-	resp, err := client.get(ctx, "/containers/changes", q, nil)
 	if err != nil {
 		return nil, err
 	}
