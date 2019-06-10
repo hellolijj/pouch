af9730a0cbfb77414eea6f5f39de84edd8f4742d
Junjun Li <junjunli666@gmail.com>
2019-06-10 15:29:32 +0800

    debug
    Signed-off-by: Junjun Li <junjunli666@gmail.com>

--- client/container_diff.go
+++ client/container_diff.go
@@ -2,15 +2,16 @@ package client
 
 import (
 	"context"
+	"fmt"
 )
 
 // ContainerDiff returns the file diffs of containers.
 func (client *APIClient) ContainerDiff(ctx context.Context, name string) ([]string, error) {
 	resp, err := client.get(ctx, "/containers/"+name+"changes", nil, nil)
-
 	if err != nil {
 		return nil, err
 	}
+	fmt.Println(resp)
 
 	diffs := []string{}
 	err = decodeBody(&diffs, resp.Body)
